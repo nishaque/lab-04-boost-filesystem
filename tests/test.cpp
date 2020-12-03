9e63a69cf2d115edd2b48181e2c6d9c2a4c9c95d
nishaque <61164463+nishaque@users.noreply.github.com>
2020-12-04 01:48:04 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,15 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Yulia Timoshenko <timoshenkojulie01@gmail.com>
 
+#include "Parser.hpp"
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+TEST(Example, EmptyTest) { EXPECT_TRUE(true); }
+TEST(Parser, PathToFile) {
+    Parser A("/Users/nishaque/Desktop/33/С++/lab-04-boost-filesystem/ftp");
+    std::cout << "broker:" << A.getBroker("00001024");
+    ASSERT_EQ(A.getBroker("00001024"), "alpha");
+}
+TEST(Parser, EmptyPathToFile) {
+    Parser A("");
+    ASSERT_EQ(A.getBroker("00001024"), "");
 }
