3a5edd39031b6ae85823d191dcd190dfd59b7a82
nishaque <61164463+nishaque@users.noreply.github.com>
2020-12-07 15:23:55 +0300

    Update Parser.hpp

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -3,27 +3,26 @@
 #define INCLUDE_PARSER_HPP_
 #include <boost/filesystem.hpp>
 #include <iostream>
-#include <string>
 #include <map>
+#include <string>
 #include <utility>
 #include <vector>
 namespace bfs = boost::filesystem;
 
-
 class Parser {
-    bfs::path pathToFtp;
-    std::map<std::string, std::pair<std::string, std::vector<std::string>>>
-            balance;
-    static bool nameIsCor(const bfs::path &y);
-    static bool isNumber(const std::string &s);
-    static std::string directory(const bfs::path &);
-    void addBalance(const std::string &name, const std::string &dir);
-    void print();
-    void cycle(const bfs::directory_entry &x);
+  bfs::path pathToFtp;
+  std::map<std::string, std::pair<std::string, std::vector<std::string>>>
+      balance;
+  static bool nameIsCor(const bfs::path &y);
+  static bool isNumber(const std::string &s);
+  static std::string directory(const bfs::path &);
+  void addBalance(const std::string &name, const std::string &dir);
+  void cycle(const bfs::directory_entry &x);
 
-public:
-    explicit Parser(std::string path);
-    std::string getBroker(std::string);
+ public:
+  explicit Parser(std::string path);
+  std::string getBroker(const std::string &);
+  friend std::ostream &operator<<(std::ostream &out, const Parser &A);
 };
 
-#endif // INCLUDE_PARSER_HPP_
+#endif  // INCLUDE_PARSER_HPP_
